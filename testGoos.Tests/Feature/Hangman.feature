3012a0c902ab4c001e948c1361ecd0cd9fd21374
cash <cash@cashwu.com>
2016-05-28 00:07:26 +0800

    init

---
+++ testGoos.Tests/Feature/Hangman.feature
@@ -0,0 +1,9 @@
+﻿Feature: Hangman
+	
+Scenario: Hangman Game
+	Given have a work "cash"
+	When game start
+	Then tries have show 12
+	And  length have show 4
+	And  used have show "AEIOU"
+	And  discovered have show "_a__"
---
+++ testGoos.Tests/Feature/HangmanSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace testGoos.Tests.Feature
+{
+    [Binding]
+    public class HangmanSteps
+    {
+        [Given(@"have a work ""(.*)""")]
+        public void GivenHaveAWork(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"game start")]
+        public void WhenGameStart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"tries have show (.*)")]
+        public void ThenTriesHaveShow(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"length have show (.*)")]
+        public void ThenLengthHaveShow(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"used have show ""(.*)""")]
+        public void ThenUsedHaveShow(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"discovered have show ""(.*)""")]
+        public void ThenDiscoveredHaveShow(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
