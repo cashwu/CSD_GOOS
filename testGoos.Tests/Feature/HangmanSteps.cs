1e78d257367d48ca8fe15b2712321a5e5d242616
cash <cash@cashwu.com>
2016-05-28 09:35:01 +0800

    commit

--- testGoos.Tests/Feature/HangmanSteps.cs
+++ testGoos.Tests/Feature/HangmanSteps.cs
@@ -1,45 +1,58 @@
-﻿using System;
+﻿using FluentAutomation;
+using FluentAutomation.Interfaces;
+using OpenQA.Selenium.Chrome;
 using TechTalk.SpecFlow;
 
 namespace testGoos.Tests.Feature
 {
     [Binding]
-    public class HangmanSteps
+    public class HangmanSteps : FluentTest
     {
+        private ChromeDriver Driver
+        {
+            get
+            {
+                return (ChromeDriver)ScenarioContext.Current["DRIVER"];
+            }
+        }
+
         [Given(@"have a work ""(.*)""")]
         public void GivenHaveAWork(string p0)
         {
-            ScenarioContext.Current.Pending();
         }
         
         [When(@"game start")]
         public void WhenGameStart()
         {
-            ScenarioContext.Current.Pending();
+            Driver.Navigate().GoToUrl("http://localhost:2938/Hangman/Index");
         }
         
         [Then(@"tries have show (.*)")]
-        public void ThenTriesHaveShow(int p0)
+        public void ThenTriesHaveShow(int tries)
         {
-            ScenarioContext.Current.Pending();
+            var b = (IActionSyntaxProvider) ScenarioContext.Current["Browser"];
+            b.Assert.Text("Tries :" + tries);
         }
         
         [Then(@"length have show (.*)")]
-        public void ThenLengthHaveShow(int p0)
+        public void ThenLengthHaveShow(int length)
         {
-            ScenarioContext.Current.Pending();
+            var b = (IActionSyntaxProvider)ScenarioContext.Current["Browser"];
+            b.Assert.Text("Length :" + length);
         }
         
         [Then(@"used have show ""(.*)""")]
-        public void ThenUsedHaveShow(string p0)
+        public void ThenUsedHaveShow(string used)
         {
-            ScenarioContext.Current.Pending();
+            var b = (IActionSyntaxProvider)ScenarioContext.Current["Browser"];
+            b.Assert.Text("Used :" + used);
         }
         
         [Then(@"discovered have show ""(.*)""")]
-        public void ThenDiscoveredHaveShow(string p0)
+        public void ThenDiscoveredHaveShow(string discovered)
         {
-            ScenarioContext.Current.Pending();
+            var b = (IActionSyntaxProvider)ScenarioContext.Current["Browser"];
+            b.Assert.Text("Discovered :" + discovered);
         }
     }
 }
---
+++ testGoos.Tests/Feature/Hooks.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace testGoos.Tests.Feature
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+            ScenarioContext.Current["DRIVER"] = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            ((ChromeDriver)ScenarioContext.Current["DRIVER"]).Quit();
+        }
+    }
+}
