a66bb83957eaabaaff84226c7f520d982d7c6700
cash <cash@cashwu.com>
2016-05-29 14:49:16 +0800

    add Hangman controller

---
+++ testGoos/Controllers/HangmanController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace testGoos.Controllers
+{
+    public class HangmanController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
